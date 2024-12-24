4b9feea58e0cdeac18888fe13cd7b3ef22ed1e6f
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-12-24 13:26:34 +0530

    juju commit

--- src/main/java/com/example/billingsystem/model/RegistrationFormDTO.java
+++ src/main/java/com/example/billingsystem/model/RegistrationFormDTO.java
@@ -3,5 +3,12 @@ package com.example.billingsystem.model;
 public class RegistrationFormDTO {
     public String fullName;
 
+    public String phoneNumber;
+
+    public String garageName;
+
+    public String garageAddress;
+
+    public String password;
 
 }
