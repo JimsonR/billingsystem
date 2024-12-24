012a881a5e8e007d81ff135dccd4558f3d45d523
JimsonR <jimsonjimmy2008@gmail.com>
2024-12-24 13:25:58 +0530

    jims commit

---
+++ src/main/java/com/example/billingsystem/entity/Adminstrator.java
@@ -0,0 +1,20 @@
+package com.example.billingsystem.entity;
+
+import jakarta.persistence.Entity;
+
+
+public class Adminstrator {
+
+    private int id;
+
+    private String fullName;
+
+    private String phoneNumber;
+
+    private String garageName;
+
+    private String garageNumber;
+
+    private String password;
+
+}
