8cdc236a2a667ec3f89f3c55a82a627cf56dec96
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-12-24 12:44:07 +0530

    juju commit

--- src/main/java/com/example/billingsystem/BillingsystemApplication.java
+++ src/main/java/com/example/billingsystem/BillingsystemApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BillingsystemApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(BillingsystemApplication.class, args);
 	}
 
--- src/main/java/com/example/billingsystem/model/RegistrationFormDTO.java
+++ src/main/java/com/example/billingsystem/model/RegistrationFormDTO.java
@@ -1,4 +1,5 @@
 package com.example.billingsystem.model;
 
 public class RegistrationFormDTO {
+    public String fullName;
 }
